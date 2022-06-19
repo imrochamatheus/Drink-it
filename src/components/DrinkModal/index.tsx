a06361422155099ea7db453283471a68368fd3b3
imrochamatheus <im.rochamatheus@gmail.com>
2022-06-18 22:44:22 -0300

    feat: Adicionado modal de detalhes

---
+++ src/components/DrinkModal/index.tsx
@@ -0,0 +1,34 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+import { useModal } from "../../providers/ModalProvider";
+
+function DrinkModal() {
+  const { isOpen, onClose } = useModal();
+
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <p>Hello</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>Close</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default DrinkModal;
