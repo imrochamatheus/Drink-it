2da53af89930c7bf4b7154d4306d979f0c667b86
imrochamatheus <im.rochamatheus@gmail.com>
2022-06-19 12:50:29 -0300

    style: Adicionadas cores padrão da aplicação ao tema do chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    black: {
+      500: "#333333",
+    },
+    yellow: {
+      500: "#ffcc00",
+    },
+    white: {
+      500: "#fafafa",
+    },
+  },
+});
