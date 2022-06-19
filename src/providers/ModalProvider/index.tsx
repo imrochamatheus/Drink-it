360692b4178653330e38509e74762d897182516d
imrochamatheus <im.rochamatheus@gmail.com>
2022-06-18 22:32:09 -0300

    feat: Adicionado modal provider

---
+++ src/providers/ModalProvider/index.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode } from "react";
+import { useDisclosure } from "@chakra-ui/react";
+
+interface ModalProviderProps {
+  children: ReactNode;
+}
+
+interface ModalContextData {
+  isOpen: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+}
+
+const ModalContext = createContext<ModalContextData>({} as ModalContextData);
+
+const ModalProvider = ({ children }: ModalProviderProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <ModalContext.Provider value={{ isOpen, onOpen, onClose }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export default ModalProvider;
