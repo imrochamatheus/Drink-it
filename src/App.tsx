8c88d5cbd2a49e6dfea25d7dc72eab2b4b0955e3
imrochamatheus <im.rochamatheus@gmail.com>
2022-06-19 21:45:08 -0300

    style: Alterado ícone da aba para logo da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import Showcase from "./components/Showcase";
-import WithSubnavigation from "./components/Nav";
-import DrinkModal from "./components/DrinkModal";
-import SearchArea from "./components/SearchArea";
-import { Box } from "@chakra-ui/react";
+import { FC } from "react";
+import Home from "./pages/Home";
 
-function App() {
-  return (
-    <Box minH={"100vh"} bg="white.500">
-      <WithSubnavigation />
-      <SearchArea />
-      <Showcase />
-      <DrinkModal />
-    </Box>
-  );
-}
+const App: FC<{}> = () => {
+  return <Home />;
+};
 
 export default App;
